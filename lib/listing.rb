cf782c180b7a5bb7299471a8ec4c601c6a1d2bb0
Natalie <49326857+natyeo@users.noreply.github.com>
2019-11-18 16:41:52 +0000

    added listing class and first test

--- lib/listing.rb
+++ lib/listing.rb
@@ -1,4 +1,21 @@
 class Listing
 
+  attr_reader :name, :description, :price, :availability
 
+  def initialize(name:, description:, price:, availability:)
+    @name = name
+    @description = description
+    @price = price
+    @availability = availability
+end
+
+  def self.all
+    if ENV['RACK'] == 'test'
+      connect = PG.connect(dbname: 'aircouch_test')
+    else
+      connect = PG.connect(dbname: 'aircouch')
+    end
+    result = connect.exec('SELECT * FROM listings;')
+    result.map { |listing| listing['name'] }
+  end
 end
--- spec/listing_spec.rb
+++ spec/listing_spec.rb
@@ -1,3 +1,16 @@
+require 'listing'
+
 describe 'Listing' do
-  
+ let(:listing) { Listing.new }
+
+ connection = PG.connect(dbname: 'aircouch_test')
+
+ describe '#all' do
+   it 'returns all listings' do
+     # listing = Listings.create(name: 'Muna couch', description: 'lovely, springy couch', price: 10, availability: '2019-11-30')
+     connection.exec("INSERT INTO listings(name, description, price, available_date) VALUES('Muna couch', 'lovely springy couch', 10, '2019-11-30');")
+     listings = Listing.all
+     expect(listings).to include "Muna couch"
+   end
+ end
 end
