bfae9a34f256350a57704a5f19c47808143c5c8c
Robert <bibbycodes@gmail.com>
2019-11-18 16:25:16 +0000

    argument bug

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -8,7 +8,7 @@ def setup_test_db
 
 end
 
-def create_tables(connect)
+def create_tables(connection)
   connection.exec("CREATE TABLE users(id SERIAL PRIMARY KEY, name VARCHAR(60), email VARCHAR(60), password VARCHAR(60));")
   connection.exec("CREATE TABLE listings(id SERIAL PRIMARY KEY, name VARCHAR(60), description VARCHAR(100), price DECIMAL(6,2), available_date DATE, user_id INTEGER REFERENCES users (id));")
   connection.exec("CREATE TABLE bookings(id SERIAL PRIMARY KEY, date DATE, listing_id INTEGER REFERENCES listings (id));")
