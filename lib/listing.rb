6bb21451076f8868fa033e3f20d687009e5a6738
Robert <bibbycodes@gmail.com>
2019-11-19 16:26:09 +0000

    added date range to listings DB

--- lib/listing.rb
+++ lib/listing.rb
@@ -2,14 +2,15 @@ require 'pg'
 
 class Listing
 
-  attr_reader :id, :name, :description, :price, :available_date
+  attr_reader :id, :name, :description, :price, :start_date, :end_date
 
-  def initialize(id, name, description, price, available_date)
+  def initialize(id, name, description, price, start_date, end_date)
     @id = id
     @name = name
     @description = description
     @price = price
-    @available_date = available_date
+    @start_date = start_date
+    @end_date = end_date
 end
 
   def self.all
@@ -19,18 +20,18 @@ end
       connect = PG.connect(dbname: 'aircouch')
     end
     result = connect.exec('SELECT * FROM listings;')
-    result.map { |listing| Listing.new(listing['id'], listing['name'], listing['description'], listing['price'], listing['available_date']) }
+    result.map { |listing| Listing.new(listing['id'], listing['name'], listing['description'], listing['price'], listing['start_date'],  listing['end_date']) }
   end
 
-  def self.create(name, description, price, available_date)
+  def self.create(name, description, price, start_date, end_date)
     if ENV['RACK'] == 'test'
       connect = PG.connect(dbname: 'aircouch_test')
     else
       connect = PG.connect(dbname: 'aircouch')
     end
-    result = connect.exec("INSERT INTO listings(name, description, price, available_date)
-                          VALUES('#{name}', '#{description}', #{price}, '#{available_date}')
-                          RETURNING id, name, description, price, available_date;")
-    Listing.new(result[0]['id'], result[0]['name'], result[0]['description'], result[0]['price'], result[0]['available_date'])
+    result = connect.exec("INSERT INTO listings(name, description, price, start_date, end_date)
+                          VALUES('#{name}', '#{description}', #{price}, '#{start_date}', '#{end_date}')
+                          RETURNING id, name, description, price, start_date, end_date;")
+    Listing.new(result[0]['id'], result[0]['name'], result[0]['description'], result[0]['price'], result[0]['start_date'], result[0]['end_date'])
   end
 end
--- spec/listing_spec.rb
+++ spec/listing_spec.rb
@@ -1,14 +1,12 @@
 require 'listing'
 
 describe 'Listing' do
-#  subject(:listing) { described_class.new(name, description, price, available_date) }
-
  connection = PG.connect(dbname: 'aircouch_test')
 
  describe '#all' do
   it 'returns all listings' do
-    connection.exec("INSERT INTO listings(name, description, price, available_date)
-                    VALUES('Muna couch', 'lovely springy couch', 10, '2019-11-30');")
+    connection.exec("INSERT INTO listings(name, description, price, start_date, end_date)
+                    VALUES('Muna couch', 'lovely springy couch', 10, '2019-11-30', '2019-12-30');")
     listings = Listing.all
     expect(listings.length).to eq 2
   end
@@ -16,7 +14,7 @@ describe 'Listing' do
 
  describe '#create' do
    it 'creates a new listing' do
-     listing = Listing.create('Nat couch', 'nice', 20, '2019-11-28')
+     listing = Listing.create('Nat couch', 'nice', 20, '2019-11-28', '2019-11-30')
      expect(listing.name).to eq 'Nat couch'
    end
  end
