154794dcf2ff4be4c7ddc0f88a4dc9d65db874fa
Arjun Roy <arjunroychoudhry4059@gmail.com>
2019-11-19 15:45:10 +0000

    Encryption Password Work

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,5 @@
+require 'bcrypt'
+
 class User
 
   attr_reader :name, :email, :password
@@ -10,6 +12,16 @@ class User
 
   def self.create(name, email, password)
     user = User.new(name, email, password)
+    encrypted_password = BCrypt::Password.create(password)
+    result = DatabaseConnection.query("INSERT INTO users(email,password) VALUES ('#{email}', '#{encrypted_password}') RETURNING id, email")
+    User.new(id: result[0]['id'], email: result[0]['email'])
   end
-
 end
+
+#   # def self.create(email:, password:)
+#   #   encrypted_password = BCrypt: :Password.create(password)
+#   #   result = DatabaseConnection.query("INSERT INTO users(email,password) VALUES ('#{email}', '#{encrypted_password}') RETURNING id, email")
+#   #   User.new(id: result[0]['id'], email: result[0]['email'])
+#   # # end
+#
+# end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -8,5 +8,11 @@ describe 'users' do
       expect(test_user.email).to eq('matt@makers.com')
       expect(test_user.password).to eq('wordpass')
     end
-  end  
+  end
+end
+describe '.create' do
+  it 'hashes the Password using BCrypt' do
+    expect(BCrypt: :Password).to receive(:create).with('password123')
+    User.create(email: 'test@example.com', password: 'password123')
+  end
 end
