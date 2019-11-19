d25c39c5dacf2f5a70499d17afd6f1e63abe88fc
Robert <bibbycodes@gmail.com>
2019-11-19 11:10:08 +0000

    added end date

--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -1,10 +1,16 @@
 require 'booking'
 
 describe Booking do
-  subject(:booking) { described_class.new(Date.new(2019,8,1))}
+  start_date = Date.new(2019,8,1)
+  end_date = Date.new(2019,8,1)
+  subject(:booking) { described_class.new(start_date, end_date)}
   context 'Initialize' do
     it "has a start date" do
       expect(booking.start_date).to be_a(Date)
+    end
+
+    it "has an end date" do
+      expect(booking.end_date).to be_a(Date)
     end
   end
 end
