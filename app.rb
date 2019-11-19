cb701c641f63053ff89734b83c38fa7d4a85f7d5
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2019-11-19 12:13:04 +0000

    sign in complete

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'sinatra/flash'
 require './lib/listing.rb'
+require './lib/user.rb'
 
 class AirCouch < Sinatra::Base
   enable :sessions
@@ -23,6 +24,21 @@ class AirCouch < Sinatra::Base
   post '/listings/new' do
     Listing.create(name: params[:name], description: params[:description], price: params[:price], available_date: params[:available_date])
     redirect '/listings'
+  end
+
+  get '/users/new' do
+    erb :users_new
+  end
+
+  post '/users/new' do
+    User.create(name: params[:name], email: params[:email], password: params[:password])
+    session[:name] = params[:name]
+    redirect '/welcome'
+  end
+
+  get '/welcome' do
+    @user = session[:name]
+    erb :welcome
   end
 
   run! if app_file == $0
